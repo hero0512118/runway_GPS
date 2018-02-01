72cbed1dd3d3d3bc3295c3cd3874cf628d36d90b
hero0512118 <hero0512118.ep05@nctu.edu.tw>
2018-02-01 15:34:01 +0800

    Initialization

---
+++ app/src/main/java/edu/nctu/runway/runway_curiosity/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.nctu.runway.runway_curiosity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
