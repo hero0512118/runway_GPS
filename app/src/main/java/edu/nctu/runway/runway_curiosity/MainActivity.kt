d09fd8ddfa3463f4e3192db1a104e308c3bfd132
hero0512118 <hero0512118.ep05@nctu.edu.tw>
2018-02-05 12:33:15 +0800

    Add onRequestPermissionsResult & Log

--- app/src/main/java/edu/nctu/runway/runway_curiosity/MainActivity.kt
+++ app/src/main/java/edu/nctu/runway/runway_curiosity/MainActivity.kt
@@ -1,25 +1,31 @@
 package edu.nctu.runway.runway_curiosity
 
 import android.Manifest
+import android.content.Intent
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.location.Location
+import android.net.Uri
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.view.View
 import android.widget.TextView
 
 import com.google.android.gms.location.LocationServices
+
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var mLastLocation : Location? =null
     var mLatitudeLabel :String? = resources.getString(R.string.latitude_label)
     var mLongitudeLabel  :String? = resources.getString(R.string.longitude_label)
     var mLatitudeText = findViewById<TextView>(R.id.latitude_text)
     var mLongitudeText = findViewById<TextView>(R.id.longitude_text)
+    private val permissionGranted = PackageManager.PERMISSION_GRANTED
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,26 +35,18 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("NewApi")
     override fun onStart() {
         super.onStart()
+
         // request_codes
         val requestCodeFineLocation = 1
-        val requestCodeCoarseLocation = 2
+
         // check permission
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED)
-        //request permission
+                != permissionGranted)
+            //request permission
             ActivityCompat.requestPermissions(this,
                     arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestCodeFineLocation)
         else getLastLocation()
-
-        /*if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED)
-        //request permission
-            ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), requestCodeCoarseLocation)
-        */
-
     }
 
     @SuppressWarnings("MissingPermission")
@@ -57,16 +55,39 @@ class MainActivity : AppCompatActivity() {
         mFusedLocationClient.lastLocation
                 .addOnCompleteListener{task ->
                     if (task.isSuccessful){
-                            mLatitudeText.text = String.Companion.format(Locale.ENGLISH,"%s : %f",
+                            mLatitudeText.text = String.format(Locale.ENGLISH,"%s : %f",
                                     mLatitudeLabel,
                                     task.result.latitude)
-                            mLongitudeText.text = String.Companion.format(Locale.ENGLISH, "%s : %f",
+                            mLongitudeText.text = String.format(Locale.ENGLISH, "%s : %f",
                                     mLongitudeLabel,
                                     task.result.longitude)
                         }else{
                             //show warning
+                            Log.w("LastLocation:exception", task.exception)
+                            Snackbar.make(findViewById(R.id.status), R.string.no_location_detected,10).show()
                         }
                 }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(grantResults.size){
+            0 -> Log.i("UserPermission","User Interaction was cancelled")
+            when(grantResults[0]) {
+                permissionGranted -> getLastLocation()
+                else -> {
+                    Snackbar.make(findViewById(R.id.status), R.string.permission_denied_explanation, 10)
+                    val onClickListener = View.OnClickListener {
+                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                        intent.data = Uri.fromParts("package", "BuildConfig.APPLICATION_ID", "null")
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    }
+                    onClickListener
+                }
+            } -> return
+        }
+
     }
 }
 
